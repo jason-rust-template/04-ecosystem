114255c57933b007d7b0af32066026ef299ea33f
yikejason <18215513386@163.com>
2024-06-30 15:22:59 +0800

    feature: introduce tokio and build a proxy

---
+++ examples/tokio1.rs
@@ -0,0 +1,31 @@
+use std::{thread, time::Duration};
+use tokio::{fs, runtime::Builder, time::sleep};
+
+fn main() {
+    let handle = thread::spawn(|| {
+        println!("hello world");
+        let rt = Builder::new_current_thread().enable_all().build().unwrap();
+
+        rt.spawn(async {
+            println!("Future 1!");
+            let content = fs::read_to_string("Cargo.toml").await.unwrap();
+            println!("content length: {}", content.len());
+        });
+
+        rt.spawn(async {
+            println!("Future 2!");
+            let ret = expensive_blocking_task("Future 2".to_string());
+            println!("result: {}", ret);
+        });
+
+        rt.block_on(async {
+            sleep(Duration::from_millis(900)).await;
+        });
+    });
+    handle.join().unwrap();
+}
+
+fn expensive_blocking_task(s: String) -> String {
+    thread::sleep(Duration::from_millis(800));
+    blake3::hash(s.as_bytes()).to_string()
+}
